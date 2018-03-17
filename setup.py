a4ff3b0dcbecf3554feea47f5cbf31639b315831
jysh1214 <jyxemperor@gmail.com>
2018-03-17 08:04:24 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='graphy',
+    version='0.1',
+    description='Lite package for graph',
+    long_description=readme,
+    author='jysh1214',
+    author_email='jyxemperor@gmail.com',
+    url='https://github.com/jysh1214/graphy',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
